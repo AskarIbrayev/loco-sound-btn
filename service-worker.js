8786299146bbfb2f7f053f839f98925c2b2fd80e
Askar Ibrayev <106813521+AskarIbrayev@users.noreply.github.com>
2025-02-10 01:57:36 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || new Response("Offline content not available", { status: 404 });
+      return response || fetch(event.request);
     })
   );
 });
