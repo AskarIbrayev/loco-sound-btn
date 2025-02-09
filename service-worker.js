43b9379fd6497543cc86e296f085958b232b9f14
Askar Ibrayev <106813521+AskarIbrayev@users.noreply.github.com>
2025-02-10 00:32:40 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,21 @@ self.addEventListener("install", (event) => {
   );
 });
 
+// Activate: Delete Old Caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
 // Serve Cached Content When Offline
 self.addEventListener("fetch", (event) => {
   event.respondWith(
